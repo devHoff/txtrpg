e015e52aa0a2010fc2a7c9919591e09b6c5e3475
Arthur Hoffmann <arthur.renck3@gmail.com>
2026-01-28 14:56:19 +0000

    Creating attack function

--- pokemon.py
+++ pokemon.py
@@ -4,11 +4,15 @@ class Pokemon:
         self.element = element
 
     def __str__(self):
-        return "Name: " + self.name + "\nElement: " + self.element + "\n"
+        return "Name: {}\nElement: {}\n".format(self.name, self.element)
 
+    def attack(self, target):
+        print("{} attacked {}!".format(self.name, target.name))
 
 my_pokemon = Pokemon("Charmander", "Fire")
 print(my_pokemon)
 
 rivals_pokemon = Pokemon("Squirtle", "Water")
 print(rivals_pokemon)
+
+my_pokemon.attack(rivals_pokemon)
