f01864e55842c58a83a82befc8dd9d20fe2e5a28
Arthur Hoffmann <arthur.renck3@gmail.com>
2026-01-28 14:49:17 +0000

    __str__ function

--- pokemon.py
+++ pokemon.py
@@ -2,10 +2,13 @@ class Pokemon:
     def __init__(self, name, element):
         self.name = name
         self.element = element
-        print("Pokemon initialized")
-        print("Name: " + name)
-        print("Element: " + element)
-        print("")
+
+    def __str__(self):
+        return "Name: " + self.name + "\nElement: " + self.element + "\n"
+
 
 my_pokemon = Pokemon("Charmander", "Fire")
+print(my_pokemon)
+
 rivals_pokemon = Pokemon("Squirtle", "Water")
+print(rivals_pokemon)
