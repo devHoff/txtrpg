81df06851fb9c273954ca8ade34242596608cff0
Arthur Hoffmann <arthur.renck3@gmail.com>
2026-01-28 15:06:02 +0000

    Levels and names

--- pokemon.py
+++ pokemon.py
@@ -1,15 +1,22 @@
 class Pokemon:
-    def __init__(self, name, element):
-        self.name = name
+    def __init__(self, species, element, level=1, name=None):
+        self.species = species
         self.element = element
+        self.level = level
+
+        #If the Pokémon has no given name, then it is called by the species name (ex: Charmander)
+        if name:
+            self.name = name
+        else:
+            self.name = species
 
     def __str__(self):
-        return "Name: {}\nElement: {}\n".format(self.name, self.element)
+        return "Name: {}\nElement: {}\nLevel: {}\n".format(self.name, self.element, self.level)
 
     def attack(self, target):
-        print("{} attacked {}!".format(self.name, target.name))
+        print("{}({}) attacked {}({})!".format(self.name, self.level, target.species, target.level))
 
-my_pokemon = Pokemon("Charmander", "Fire")
+my_pokemon = Pokemon("Charmander", "Fire", name = "João")
 print(my_pokemon)
 
 rivals_pokemon = Pokemon("Squirtle", "Water")
