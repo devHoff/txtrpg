037617a5fa83d4429e7f1b1be9627dd985a18cec
Arthur Hoffmann <arthur.renck3@gmail.com>
2026-01-28 14:43:41 +0000

    Creating class "Pokemon" my_pokemon and rivals_pokemon example objects

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,11 @@
-print("Hello world")
+class Pokemon:
+    def __init__(self, name, element):
+        self.name = name
+        self.element = element
+        print("Pokemon initialized")
+        print("Name: " + name)
+        print("Element: " + element)
+        print("")
+
+my_pokemon = Pokemon("Charmander", "Fire")
+rivals_pokemon = Pokemon("Squirtle", "Water")
